4c151c45cc38539572e536196f3aec7823e13d61
Nathan Szelag <nszelag@live.com>
2019-09-24 22:36:35 +0000

    updated knex variables

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
@@ -8,7 +9,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
