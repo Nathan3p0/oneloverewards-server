e2c1a87f1df8dbc1f2454a5ad1957238ad202fb2
Nathan Szelag <nszelag@live.com>
2019-09-24 06:25:33 +0000

    added auth route

---
+++ src/auth/auth-router.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const AuthService = require('./auth-service')
+const authRouter = express.Router()
+const jsonBodyParser = express.json()
+
+authRouter.post('/login', jsonBodyParser, (req, res, next) => {
+    const { username, password } = req.body
+    const loginUser = { username, password }
+
+    for (const [key, value] of Object.entries(loginUser)) {
+        if (value == null) {
+            return res.status(400).json({
+                error: `Missing ${key} in request body`
+            })
+        }
+    }
+
+    AuthService.getUserWithUserName(
+        req.app.get('db'), loginUser.username
+    )
+        .then(user => {
+            if (!user) {
+                return res.status(400).json({ error: 'Incorrect Username or Password' })
+            }
+
+            return AuthService.comparePassword(
+                loginUser.password, user.password
+            )
+                .then(passwordsMatch => {
+                    if (!passwordsMatch) {
+                        return res.status(400).json({
+                            error: 'Incorrect Username or Password'
+                        })
+                    }
+
+                    const sub = user.username
+
+                    const payload = {
+                        staff_id: user.id
+                    }
+
+                    res.send({
+                        authToken: AuthService.createJwt(sub, payload)
+                    })
+                })
+        })
+        .catch(next)
+})
+
+module.exports = authRouter
